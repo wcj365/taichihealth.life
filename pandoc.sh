07fc3884a99e8a5a2906bb2d3798c8107d5b002e
Dr. Chaojie Wang <wcj365@gmail.com>
2022-11-20 22:29:13 +0000

    Built the static website of the books.

--- pandoc.sh
+++ pandoc.sh
@@ -2,34 +2,16 @@
 
 # Step 1. Transform the contents for Pandoc
 
-if [ -d "_pandoc" ] 
-then
-    rm -r _pandoc 
-fi
-
-mkdir _pandoc
+python pandoc.py
 
 # Step 2. Generate PDF version of the book
 
-
-BOOKS="01_classic_poems 02_modern_poems 03_proses 04_english"
-
-for book in $BOOKS
-do
-    python pandoc.py src/$book _pandoc/$book
-
-    if [ "$book" = "01_classic_poems" ]
-    then
-        python classic_poems.py    # (序，自序等放在第一册格律诗中)
-    fi
-
-    pandoc --pdf-engine=xelatex  `find _pandoc/$book -name '*.md' | sort` -o docs/offline/$book.pdf
-    pandoc --pdf-engine=xelatex  `find _pandoc/$book -name '*.md' | sort` -o docs/offline/$book.epub
-    pandoc --pdf-engine=xelatex  `find _pandoc/$book -name '*.md' | sort` -o docs/offline/$book.docx
-done
+pandoc --pdf-engine=xelatex `find _pandoc -name '*.md' | sort` -o docs/offline/taiji.pdf
+pandoc --pdf-engine=xelatex `find _pandoc -name '*.md' | sort` -o docs/offline/taiji.epub
+pandoc --pdf-engine=xelatex `find _pandoc -name '*.md' | sort` -o docs/offline/taiji.docx
 
 # Step 3. Push the changes to GitHub
 
 git add .
-git commit -m "Built the offline versions of the books (pdf | epub | docx)."
+git commit -m "Built the PDF verion of the book."
 git push
